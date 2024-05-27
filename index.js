2d3abaa75bd06cea23832f038eb216927cd94cdb
inspirationjon <ilkhomjon.interview@gmail.com>
2024-05-27 12:19:02 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// This is an input class. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateBst(tree) {
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.BST = BST;
+exports.validateBst = validateBst;
