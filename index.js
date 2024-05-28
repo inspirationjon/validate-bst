61b4456442bce04cf02557b049df1c21846fa534
inspirationjon <ilkhomjon.interview@gmail.com>
2024-05-28 15:07:35 -0500

    Comment

--- index.js
+++ index.js
@@ -7,18 +7,26 @@ class BST {
   }
 }
 
+// time - O(n), because we are validating everthing once
+// space - O(h), memory used each time recursive is on
+
 function validateBst(tree) {
+  // Passing min, max values
   return validateBstHelper(tree, -Infinity, Infinity);
 }
 
 function validateBstHelper(tree, minValue, maxValue) {
+  // Meaning tree fully validated
   if (tree === null) return true;
 
+  // Not BST, value is not btw the range
   if (tree.value < minValue || tree.value >= maxValue) return false;
 
+  // Check left and right
   const isLeftValid = validateBstHelper(tree.left, minValue, tree.value);
   const isRightValid = validateBstHelper(tree.right, tree.value, maxValue);
 
+  // Return result based on the validations
   return isLeftValid && isRightValid;
 }
 
